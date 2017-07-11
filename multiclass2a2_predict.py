b00ce2fac673a21e4936b3c777b86ed521a56c62
LucasCosta <lucas.costa@wdatecnologia.com.br>
2017-07-11 17:02:10 -0300

    anotacoes em 11 de julho

--- multiclass2a2_predict.py
+++ multiclass2a2_predict.py
@@ -13,24 +13,29 @@ def ler_arquivo(address):
                 obj = obj+1                                             ##
         return bd  
 qtc = 2
-maior = 0.80
-clss = [2,3]
+cls1 = [1,4]
+cls2 = [2,3]
+maior = 0.915
 tamanhos = np.zeros(qtc)
 Teste = []
-for j,i in enumerate(clss):
-        ant = len(Teste)
+for j,i in enumerate(cls1):
         Teste += ler_arquivo("Base Multiclasse/classe_{}.txt".format(i))
-        tamanhos[j] = len(Teste) - ant
+tamanhos[0] = len(Teste)
+for j,i in enumerate(cls2):
+        Teste += ler_arquivo("Base Multiclasse/classe_{}.txt".format(i))
+tamanhos[1] = len(Teste) - tamanhos[0]
+
 print tamanhos
 labels = []
 #print tamanhos
 for k,i in enumerate(tamanhos):
         for j in range(int(i)):
                 labels.append(k)
-oRodada = rodada(500, qtc, nTreino=50, nAtrib=12, amostra=200)
+oRodada = rodada(500, qtc, nTreino=100, nAtrib=12, amostra=400)
 
 for i,j in enumerate(Teste):
         oRodada.GLCM.add_objeto(Teste[i]+[labels[i]+1])
+        
 params = dict(kernel_type = cv2.SVM_RBF,
                   svm_type = cv2.SVM_C_SVC,
                   C=9.0,
@@ -44,12 +49,13 @@ self.clean()
 positions = [x for x in range(1,13)]
 for i in range(self.num_ite):
         print i
-        self.iteracoes[i].conj_treino,self.iteracoes[i].conj_teste = self.GLCM.extraiTp1(150, 50, qtc)
+        self.iteracoes[i].conj_treino,self.iteracoes[i].conj_teste = self.GLCM.extraiTp1(300, 100, qtc)
         train = []
         trainLabel =[]
         for k in self.iteracoes[i].conj_treino:
                 train.append(self.GLCM.getNewAtrib(k,positions))
                 trainLabel.append(self.GLCM.labels[k])
+        
         if params is None:
                 self.iteracoes[i].svm_params = dict(kernel_type = cv2.SVM_RBF,svm_type = cv2.SVM_C_SVC,gamma=2.0)
         else: self.iteracoes[i].svm_params = params
@@ -63,7 +69,7 @@ for i in range(self.num_ite):
         self.iteracoes[i].set_acuracia()
         self.sum_cfm = np.add(self.sum_cfm,self.iteracoes[i].dados) 
         if self.iteracoes[i].acuracia[2][0]> maior:
-                svm.save("Vetores_svm2_3.txt")
+                svm.save("Vetores_svm14_23.txt")
                 maior = self.iteracoes[i].acuracia[2][0]
                 print "salvou",maior
 Salvar_texto(str(self),"resultados_multiclasse_4_50teste_200treino.txt")
@@ -73,4 +79,3 @@ for i in self.get_avg_cfm():
         for j in i:
                 string += "\t{:02.02f}".format(j)
         print string
-                
