1e21747214eeea2d8b95fc0e06e066918d4c6e38
lukkascost <lucas.costa@lit.ifce.edu.br>
2017-11-02 19:34:51 -0300

    files with 1-7b GLCM atributes

--- GLCM_DATA_N_BITS.py
+++ GLCM_DATA_N_BITS.py
@@ -1,18 +1,29 @@
 from MachineLearn.Classes.Extractors.GLCM import *
 
-fname = "../isolador-multiclasse/base/tempo/filtrado_lucas/Classe_0.txt.gz"
-nbits = 12
-
-print "Creating array of objects..."
-glArray = np.array([GLCM(np.matrix(x), nbits) for x in np.loadtxt(fname, delimiter=",")],dtype="object")
-
-print "generate co occurence matrix..."
-[gl.generateCoOccurenceHorizontal() for gl in glArray]
-
-print "Normalizing co occurence ..."
-[gl.normalizeCoOccurence() for gl in glArray]
-
-print "Calculating attributes..."
-[gl.calculateAttributes() for gl in glArray]
-
-print "Finish!"
+for nbits in range(1,9):
+        arrayGLCM = np.zeros((0,25))            
+        for i in range(5):
+                fname = "../isolador-multiclasse/base/tempo/filtrado_lucas/{}b/Classe_{}.txt.gz".format(nbits,i)
+                
+                print "Creating array of objects..."
+                glArray = np.array([GLCM(np.matrix(x), nbits) for x in np.loadtxt(fname, delimiter=",")],dtype="object")
+                
+                print "generate co occurence matrix..."
+                [gl.generateCoOccurenceHorizontal() for gl in glArray]
+                
+                print "Normalizing co occurence ..."
+                [gl.normalizeCoOccurence() for gl in glArray]
+                
+                print "Calculating attributes..."
+                [gl.calculateAttributes() for gl in glArray]
+                
+                print "Exporting attributes"
+                for gl in glArray:
+                        arrayGLCM = np.vstack((arrayGLCM,gl.exportToClassfier(float(i+1)))) 
+                
+                print "Finish!"
+                
+                print arrayGLCM
+                print "\n"
+        np.savetxt("GLCM_FILES/{}b.txt.gz".format(nbits),arrayGLCM,delimiter = ",", fmt="%.10e")
+        print "File Generated successful"
