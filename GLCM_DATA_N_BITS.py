89fa26739851aa9d197bad3a44245c731901ae42
lukkascost <lucas.costa@lit.ifce.edu.br>
2017-11-03 14:56:49 -0300

    File with 8 bit GLCM

--- GLCM_DATA_N_BITS.py
+++ GLCM_DATA_N_BITS.py
@@ -1,12 +1,15 @@
 from MachineLearn.Classes.Extractors.GLCM import *
 
-for nbits in range(1,9):
+for nbits in [8]:
         arrayGLCM = np.zeros((0,25))            
-        for i in range(5):
+        for i in [0,1,2,3,4]:
                 fname = "../isolador-multiclasse/base/tempo/filtrado_lucas/{}b/Classe_{}.txt.gz".format(nbits,i)
+                array = np.loadtxt(fname, delimiter=",")
+                array[139,1477] = 255
+                print array[array>=256]
                 
                 print "Creating array of objects..."
-                glArray = np.array([GLCM(np.matrix(x), nbits) for x in np.loadtxt(fname, delimiter=",")],dtype="object")
+                glArray = np.array([GLCM(np.matrix(x), nbits) for x in array],dtype="object")
                 
                 print "generate co occurence matrix..."
                 [gl.generateCoOccurenceHorizontal() for gl in glArray]
